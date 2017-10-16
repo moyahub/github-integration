c2454a8901f1ba2f0793d8f5578378bde67ae985
moya <moyayam@gmail.com>
2017-10-16 11:19:47 +0200

    My First commit

---
+++ src/main/java/com/moya/Runner.java
@@ -0,0 +1,22 @@
+package com.moya;
+
+import javax.swing.*;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        // create a pooup window message
+        JOptionPane.showMessageDialog(null,
+                "Welocome on Git",
+                "Information",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        //we will add screen to resource, you will ee late why!
+        //create an 'screen' folder under er resource folder
+        // then add a screen1.png into screen folder
+        // then we will export project to GitHub using men  item
+        // VCS | Import to Version Control | Share Project On GitHub
+        // It's important to not send .idea directory and .iml file.
+        // these are intellij IDEA config files.
+    }
+}
